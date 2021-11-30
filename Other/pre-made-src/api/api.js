72bd8623e40c6c6380c73ca44d2a07150cab8e9b
Maria Nidchenko <marianidchenko@gmail.com>
2021-11-30 19:06:48 +0200

    testing api

---
+++ Other/pre-made-src/api/api.js
@@ -0,0 +1,91 @@
+import { clearUserData, getUserData, setUserData } from "../util.js";
+
+async function request(url, options) {
+    const response = await fetch(url, options);
+    try {
+        if (response.ok == false) {
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        if (response.status == 204) {
+            return response;
+        } else {
+            return response.json();
+        }
+
+    } catch (err) {
+        alert(err.message);
+        throw err;
+    }
+}
+
+function createOptions(method = 'get', data) {
+    const options = {
+        method,
+        headers: {},
+    };
+
+    if ( data != undefined ) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data)
+    }
+
+    const userData = getUserData();
+    if (userData != null) {
+        options.headers['X-Authorization'] = userData.token;
+    }
+
+    return options;
+}
+
+export async function get(url) {
+    return request(url, createOptions());
+}
+
+export async function post(url, data) {
+    return request(url, createOptions('post', data));
+}
+
+export async function put(url, data) {
+    return request(url, createOptions('put', data));
+}
+
+export async function del(url) {
+    return request(url, createOptions('delete'));
+}
+
+export async function login(email, password) {
+    const response = await post('/users/login', { email, password });
+
+    const userData = {
+        username: response.username,
+        email: response.email,
+        id: response._id,
+        gender: response.gender,
+        token: response.accessToken
+    };
+
+    setUserData(userData);
+    return response;
+}
+
+export async function register(username, email, password, gender) {
+    const response = await post('/users/register', { username, email, password, gender });
+
+    const userData = {
+        username: response.username,
+        email: response.email,
+        id: response._id,
+        gender: response.gender,
+        token: response.accessToken
+    };
+
+    setUserData(userData);
+    return response;
+}
+
+export async function logout() {
+    await get('/users/logout');
+    clearUserData();
+}
